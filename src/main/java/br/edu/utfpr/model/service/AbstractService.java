9c24611f69f339a5783c91ce5a7cf16f65379f1b
ronifabio <roni.banaszewski@gmail.com>
2016-11-22 11:05:41 -0200

    Importação do jQuery do PrimeFaces e retornos de status das operações CRUD

--- src/main/java/br/edu/utfpr/model/service/AbstractService.java
+++ src/main/java/br/edu/utfpr/model/service/AbstractService.java
@@ -5,88 +5,84 @@ import br.edu.utfpr.model.dao.AbstractDAO;
 import br.edu.utfpr.util.JPAUtil;
 
 public class AbstractService<PK, T> {
-	
-	protected AbstractDAO<PK, T> dao;
-	
-	public void save(T entity){
-        try{
-            JPAUtil.beginTransaction();            
+
+    protected AbstractDAO<PK, T> dao;
+
+    public boolean save(T entity) {
+        try {
+            JPAUtil.beginTransaction();
             dao.save(entity);
-            JPAUtil.commit();
-        }
-        catch(Exception e){            
+            JPAUtil.commit();            
+        } catch (Exception e) {
             JPAUtil.rollBack();
+            return false;
+        } finally {
+            JPAUtil.closeEntityManager();
         }
-        finally{
-			JPAUtil.closeEntityManager();
-		}
+        return true;
     }
-	
-	public void update(T entity){
-        try{
-            JPAUtil.beginTransaction();            
+
+    public boolean update(T entity) {
+        try {
+            JPAUtil.beginTransaction();
             dao.update(entity);
             JPAUtil.commit();
-        }
-        catch(Exception e){            
+        } catch (Exception e) {
             JPAUtil.rollBack();
+            return false;
+        } finally {
+            JPAUtil.closeEntityManager();
         }
-        finally{
-			JPAUtil.closeEntityManager();
-		}
+        return true;
     }
-	
-	public void delete(T entity){
-        try{
-            JPAUtil.beginTransaction();            
+
+    public boolean delete(T entity) {
+        try {
+            JPAUtil.beginTransaction();
             dao.delete(entity);
             JPAUtil.commit();
-        }
-        catch(Exception e){            
+        } catch (Exception e) {
             JPAUtil.rollBack();
+            return false;
+        } finally {
+            JPAUtil.closeEntityManager();            
         }
-        finally{
-			JPAUtil.closeEntityManager();
-		}
+        return true;
     }
-	
-	public T getById(PK pk){
-		T entity = null;
-		
-		try{
-            JPAUtil.beginTransaction();            
+
+    public T getById(PK pk) {
+        T entity = null;
+
+        try {
+            JPAUtil.beginTransaction();
             entity = dao.getById(pk);
             JPAUtil.commit();
-        }
-        catch(Exception e){            
+        } catch (Exception e) {
             JPAUtil.rollBack();
+        } finally {
+            JPAUtil.closeEntityManager();
         }
-        finally{
-			JPAUtil.closeEntityManager();
-		}
-		
-		return entity;
-	}
-	
-	public T getByProperty(String propertyName, String propertyValue){
-		T entity = null;		
-		try{
-            JPAUtil.beginTransaction();            
+
+        return entity;
+    }
+
+    public T getByProperty(String propertyName, String propertyValue) {
+        T entity = null;
+        try {
+            JPAUtil.beginTransaction();
             entity = dao.getByProperty(propertyName, propertyValue);
             JPAUtil.commit();
-        }
-        catch(Exception e){            
+        } catch (Exception e) {
             JPAUtil.rollBack();
             e.printStackTrace();
+        } finally {
+            JPAUtil.closeEntityManager();
         }
-        finally{
-			JPAUtil.closeEntityManager();
-		}			
-		
-		return entity;
-	}
-	
-	public List<T> findAll(){
+
+        return entity;
+    }
+
+    public List<T> findAll() {
         return dao.findAll();
     }
 }
